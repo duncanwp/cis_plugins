c3328e5794d3f0395596db228a237d5e07d1daa9
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2016-03-08 16:36:49 +0000

    Fixing air pressure units

--- gassp.py
+++ gassp.py
@@ -19,3 +19,37 @@ class GASSP(NCAR_NetCDF_RAF):
         super(NCAR_NetCDF_RAF, self).__init__()
         self.variableSelectorClass = variable_selector_class
         self.valid_dimensions = ["Time"]
+
+    def _create_coord(self, coord_axis, data_variable_name, data_variables, standard_name):
+        """
+        Create a coordinate for the co-ordinate list
+        :param coord_axis: axis of the coordinate in the coords
+        :param data_variable_name: the name of the variable in the data
+        :param data_variables: the data variables
+        :param standard_name: the standard name it should have
+        :return: a coords object
+        """
+        from cis.data_io.netcdf import get_metadata
+        from cis.data_io.Coord import Coord
+        from cf_units import Unit
+        import logging
+
+        coordinate_data_objects = []
+        for d in data_variables[data_variable_name]:
+            m = get_metadata(d)
+            m.alter_standard_name(standard_name)
+            if standard_name == 'air_pressure':
+                if ',' in m.units:
+                    cfunit = Unit(m.units.split(',')[0])
+                else:
+                    Unit(m.units)
+                logging.info("Parsed air pressure units '''{old}''' as {new} ".format(old=m.units, new=cfunit))
+                logging.info('Converting to hPa')
+                data = Unit(m.units).convert(d[:], 'hPa')
+                m.units = 'hPa'
+            else:
+                data = d
+            coordinate_data_objects.append(Coord(data, m, coord_axis))
+
+
+        return Coord.from_many_coordinates(coordinate_data_objects)
