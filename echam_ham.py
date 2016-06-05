21902146571d681ec68f0e3ef954d730da93964d
Duncan Watson-Parris <duncan@watson-parris.co.uk>
2016-06-05 11:34:51 +0100

    Making echam_ham air_pressure field hPa by default

--- echam_ham.py
+++ echam_ham.py
@@ -46,6 +46,11 @@ class ECHAM_HAM(NetCDF_Gridded):
 
     def _add_available_aux_coords(self, cube, filenames):
         from iris.aux_factory import HybridPressureFactory
+
+        # First convert the hybrid coefficients to hPa, so that air pressure will be in hPa
+        cube.coord('hybrid A coefficient at layer midpoints').convert_units('hPa')
+        cube.coord('surface pressure').convert_units('hPa')
+
         if len(cube.coords(long_name='hybrid level at layer midpoints')) > 0:
             cube.add_aux_factory(HybridPressureFactory(delta=cube.coord('hybrid A coefficient at layer midpoints'),
                                                        sigma=cube.coord('hybrid B coefficient at layer midpoints'),
