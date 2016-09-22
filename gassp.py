b395aa68ef219cfd854795059141f953e3065b1a
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2016-09-22 08:45:53 +0100

    Rolling back netcdf interface changes to match with CIS master

--- gassp.py
+++ gassp.py
@@ -60,7 +60,7 @@ class GASSP(NCAR_NetCDF_RAF):
         """
         super(GASSP, self).__init__(variable_selector_class=variable_selector_class)
 
-    def _create_coord(self, coord_axis, data_variable_name, filenames, standard_name):
+    def _create_coord(self, coord_axis, data_variable_name, data_variables, standard_name):
         """
         Create a coordinate for the co-ordinate list
         :param coord_axis: axis of the coordinate in the coords
@@ -69,16 +69,16 @@ class GASSP(NCAR_NetCDF_RAF):
         :param standard_name: the standard name it should have
         :return: a coords object
         """
-        from cis.data_io.netcdf import get_metadata, read
+        from cis.data_io.netcdf import get_metadata, get_data
         from cis.data_io.Coord import Coord
         from cf_units import Unit
         import logging
 
         coordinate_data_objects = []
-        for f in filenames:
-            m = get_metadata(data_variable_name, f)
+        for d in data_variables[data_variable_name]:
+            data = get_data(d)
+            m = get_metadata(d)
             m.standard_name = standard_name
-            data = read(f, data_variable_name)[data_variable_name]
             if standard_name == 'air_pressure':
                 if ',' in m.units:
                     m.units = m.units.split(',')[0]
@@ -95,4 +95,3 @@ class GASSP(NCAR_NetCDF_RAF):
             coordinate_data_objects.append(Coord(data, m, coord_axis))
 
         return Coord.from_many_coordinates(coordinate_data_objects)
-
