332b44e712bbb9469986f9322c8b203b232cd7a9
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2017-03-28 16:27:00 +0100

    Minor fix

--- cis_hpa.py
+++ cis_hpa.py
@@ -8,13 +8,13 @@ from cf_units import Unit
 class cis_hpa(cis):
 
     def create_coords(self, filenames, usr_variable=None):
-        o = super(cis_hpa, self).create_coords(filenames, usr_variable)
+        o = cis.create_coords(self, filenames, usr_variable)
 
         try:
             air_pressure_coord = o.coord('air_pressure')
         except:
             pass
         else:
-            if air_pressure_coord.unit == 'hpa':
-                air_pressure_coord.unit = Unit('hPa')
+            if air_pressure_coord.units == 'hpa':
+                air_pressure_coord.units = Unit('hPa')
         return o
