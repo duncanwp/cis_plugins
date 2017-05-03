c1990cd9427479525749174264a9e5d8362e3a37
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2017-05-03 15:36:34 +0100

    Adding routines for reading column averaged variables

--- caliop_v4.py
+++ caliop_v4.py
@@ -19,12 +19,67 @@ class Caliop_V4(Caliop_L2):
         # sub-Level-2-resolution bin.
         return self._get_calipso_data(sds)[:, :, 0]
 
+    def _create_one_dimensional_coord_list(self, filenames, index_offset=1):
+        """
+        Create a set of coordinates appropriate for a ond-dimensional (column integrated) variable
+        :param filenames:
+        :param int index_offset: For 5km products this will choose the coordinates which represent the start (0),
+        middle (1) and end (2) of the 15 shots making up each column retrieval.
+        :return:
+        """
+        from pyhdf.error import HDF4Error
+        from cis.data_io import hdf_sd
+        import datetime as dt
+        from cis.time_util import convert_sec_since_to_std_time, cis_standard_time_unit
+
+        variables = ['Latitude', 'Longitude', "Profile_Time"]
+        logging.info("Listing coordinates: " + str(variables))
+
+        # reading data from files
+        sdata = {}
+        for filename in filenames:
+            try:
+                sds_dict = hdf_sd.read(filename, variables)
+            except HDF4Error as e:
+                raise IOError(str(e))
+
+            for var in list(sds_dict.keys()):
+                utils.add_element_to_list_in_dict(sdata, var, sds_dict[var])
+
+        # latitude
+        lat_data = hdf.read_data(sdata['Latitude'], self._get_calipso_data)[:, index_offset]
+        lat_metadata = hdf.read_metadata(sdata['Latitude'], "SD")
+        lat_coord = Coord(lat_data, lat_metadata, 'Y')
+
+        # longitude
+        lon = sdata['Longitude']
+        lon_data = hdf.read_data(lon, self._get_calipso_data)[:, index_offset]
+        lon_metadata = hdf.read_metadata(lon, "SD")
+        lon_coord = Coord(lon_data, lon_metadata, 'X')
+
+        # profile time, x
+        time = sdata['Profile_Time']
+        time_data = hdf.read_data(time, self._get_calipso_data)[:, index_offset]
+        time_data = convert_sec_since_to_std_time(time_data, dt.datetime(1993, 1, 1, 0, 0, 0))
+        time_coord = Coord(time_data, Metadata(name='Profile_Time', standard_name='time', shape=time_data.shape,
+                                               units=cis_standard_time_unit), "T")
+
+        # create the object containing all coordinates
+        coords = CoordList()
+        coords.append(lat_coord)
+        coords.append(lon_coord)
+        coords.append(time_coord)
+
+        return coords
+
     def create_data_object(self, filenames, variable):
         logging.debug("Creating data object for variable " + variable)
 
         # reading coordinates
-        # the variable here is needed to work out whether to apply interpolation to the lat/lon data or not
-        coords = self._create_coord_list(filenames)
+        if variable.startswith('Column'):
+            coords = self._create_one_dimensional_coord_list(filenames, index_offset=1)
+        else:
+            coords = self._create_coord_list(filenames, index_offset=1)
 
         # reading of variables
         sdata, vdata = hdf.read(filenames, variable)
