8d535be76e64ab484b72788217dd3e7d0425607c
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2017-08-04 16:54:00 +0100

    Fix for files with no surface pressure coordinate (e.g. vphysc)

--- echam_ham_pascals.py
+++ echam_ham_pascals.py
@@ -95,11 +95,24 @@ class ECHAM_HAM_Pascals(NetCDF_Gridded):
 
     def _add_available_aux_coords(self, cube, filenames):
         from iris.aux_factory import HybridPressureFactory
+        from iris.coords import AuxCoord
+        from iris.exceptions import CoordinateNotFoundError
+        import iris
+
+        try:
+            surface_pressure = cube.coord('surface pressure')
+        except iris.exceptions.CoordinateNotFoundError as e:
+            # If there isn't a surface pressure coordinate we can try and pull out the lowest pressure level
+            surface_pressure_cubes = iris.load(filenames, 'atmospheric pressure at interfaces',
+                                               callback=self.load_multiple_files_callback)
+            surface_pressure_cube = surface_pressure_cubes.concatenate_cube()[:, -1, :, :]
+            surface_pressure = AuxCoord(points=surface_pressure_cube.data, long_name='surface pressure', units='Pa')
+            cube.add_aux_coord(surface_pressure, (0, 2, 3))
 
         if len(cube.coords(long_name='hybrid level at layer midpoints')) > 0:
             cube.add_aux_factory(HybridPressureFactory(delta=cube.coord('hybrid A coefficient at layer midpoints'),
                                                        sigma=cube.coord('hybrid B coefficient at layer midpoints'),
-                                                       surface_air_pressure=cube.coord('surface pressure')))
+                                                       surface_air_pressure=surface_pressure))
 
     def get_file_type_error(self, filename):
         """
