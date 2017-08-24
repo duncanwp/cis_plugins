62363435c75b9d84f59bbadc3f339e115791fe68
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2017-08-24 12:17:22 +0100

    Adding some safety checks

--- multi-netcdf.py
+++ multi-netcdf.py
@@ -9,7 +9,7 @@ class multi_netcdf(NetCDF_Gridded):
     @staticmethod
     def load_multiple_files_callback(cube, field, filename):
         # We need to remove the history field when reading multiple files so that the cubes can be properly merged
-        cube.attributes.pop('history')
+        cube.attributes.pop('history', None)
         return cube
 
     def create_data_object(self, filenames, variable):
@@ -21,7 +21,8 @@ class multi_netcdf(NetCDF_Gridded):
         from iris.aux_factory import HybridPressureFactory
         # Add the derived coordinates back (https://github.com/SciTools/iris/issues/2478)...
         cube = super().create_data_object(filenames, variable)
-        cube.add_aux_factory(HybridPressureFactory(cube.coord('atmosphere_hybrid_sigma_pressure_coordinate'),
-                                                   cube.coord('hybrid B coefficient at layer midpoints'),
-                                                   cube.coord('surface pressure')))
+        if cube.coords('atmosphere_hybrid_sigma_pressure_coordinate') and not cube.coords('air_pressure'):
+            cube.add_aux_factory(HybridPressureFactory(cube.coord('atmosphere_hybrid_sigma_pressure_coordinate'),
+                                                       cube.coord('hybrid B coefficient at layer midpoints'),
+                                                       cube.coord('surface pressure')))
         return cube
